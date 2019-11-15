19283f3cd9a7d79f5f63a127a168e2e301d8d38a
Patricia Tran <ptran5@wisc.edu>
2019-11-15 12:18:01 -0600

    My personal edits

---
+++ mendota/PQT/glm_trial_mendota_PQT.R
@@ -0,0 +1,41 @@
+## Clear workspace and console
+rm(list=ls()); cat("\014")
+# Load required packages
+#library(FSA); library(dplyr); library(magrittr);library(lubridate)
+library(ggplot2)
+
+#library(devtools)
+
+#devtools::install_github('USGS-R/glmtools',ref='ggplot_overhaul')
+#devtools::install_github('GLEON/GLM3r')
+
+library(glmtools)
+library(GLM3r)
+
+setwd("~/Documents/GLM955-South/mendota/PQT/")
+sim_folder = getwd()
+
+run_glm()
+
+out_file = file.path(sim_folder, 'outputs/output.nc')
+
+pdf("Simulations/Temp-Salt4.pdf", width=11, height=8.5) 
+plot_var(nc_file = out_file, var_name='temp')
+plot_var(nc_file = out_file, var_name='salt')
+dev.off()
+
+
+plot_var_compare(nc_file = out_file, field_file = 'field_mendota.csv', var_name = 'temp')
+
+temp_rmse = compare_to_field(out_file, field_file = 'field_mendota.csv', metric='water.temperature', as_value=F)
+print(paste(round(temp_rmse,2),'deg C RMSE'))
+
+# I get 1.48 RMSE
+
+# 1.5 = very good fit - so 1.55 is good - this was achieved just with base value
+# base value: wind_factor=1.0, kw (Light extinction)= 0.22, Lw_factor=0.91
+
+# change values: wind_factor=0.9 -- RMSE = 1.37
+# change values: kw =0.24 -- RMSE = 1.45
+# change values: kw = 0.20, RMSE = 1.67
+# change values: wind_factor = 0.9, kw = 0.24 -- RMSE = 1.37
